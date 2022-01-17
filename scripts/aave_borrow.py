1124a8b68d921a51b15692b838651bec4281ee9f
Spahr Michael <spahr.mike@protonmail.com>
2022-01-17 23:56:08 +0100

    add function to borrow

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -33,6 +33,33 @@ def approve_erc20(amount, spender, erc20_address, account):
     print("Approved")
 
 
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited!")
+    print(f"You have {total_debt_eth} worth of ETH of debt!")
+    print(f"You can borrow {available_borrow_eth} worth of ETH!")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+def get_asset_price(price_feed_address):
+    # ABI, Address
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The dai/eth price is {converted_latest_price}")
+    return float(converted_latest_price)
+
+
 def main():
     account = get_account()
     erc20_address = config["networks"][network.show_active()]["weth_token"]
@@ -45,9 +72,33 @@ def main():
     lending_pool = get_lending_pool()
     # approve sending out ERC20 tokens
     approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
+    # Deposit
     print("Depositing...")
     deposit_tx = lending_pool.deposit(
         erc20_address, AMOUNT, account.address, 0, {"from": account}
     )
     deposit_tx.wait(1)
     print("Deposited")
+    # how much can we borrow
+    print("Calculating how much to borrow...")
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    # borrowable_eth -> borrowable_dai*95%
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI!")
+    # Borrow
+    print("Borrowing...")
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("Borrowed DAI")
+    get_borrowable_data(lending_pool, account)
